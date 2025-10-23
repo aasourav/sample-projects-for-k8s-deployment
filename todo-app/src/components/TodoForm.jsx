61c925423c924747eb31d14d89ed244dc2c7595e
Ahsan Amin <ahsan.sourav109@gmail.com>
2025-10-23 15:39:47 +0600

    Update TodoForm.jsx

--- todo-app/src/components/TodoForm.jsx
+++ todo-app/src/components/TodoForm.jsx
@@ -37,7 +37,7 @@ const TodoForm = ({ onAdd }) => {
     if (!title) return;
 
     try {
-        const response = await axios.post('/create', { title });
+        const response = await axios.post('http://localhost:3000/create', { title });
         console.log("HIHI",response.data) 
       onAdd(response.data);
       setTitle('');
